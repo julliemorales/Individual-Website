0a34707b1d6d3249679cd509d8123ff3e838b881
Jullie Angeli Morales <jullieangelimorales05@gmail.com>
2022-08-27 01:45:09 +0800

    Created section founder & added scroll animation

--- js/index.js
+++ js/index.js
@@ -1,18 +1,30 @@
 $(document).ready(function () {
   $(".carousel").slick({
+    arrows: false,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
     dots: true,
     draggable: true,
-  });
-  $(".carousel-md").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots: true,
-    draggable: true,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          arrows: true,
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          arrows: true,
+        },
+      },
+    ],
   });
 });
+
+AOS.init();
